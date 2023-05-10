b1fb7b1a68b9245ebc9c860d306009eb7df3b9c3
ADMichnik <amek995@gmail.com>
2023-05-10 12:36:42 +0200

    created recipe model, added recipe items into list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe (`A Test Recipe`, `That is simply a test`, `https://upload.wikimedia.org/wikipedia/commons/9/96/Low-Carb_Zoodles_with_Bolognese.jpg`),
+    new Recipe (`Cauliflower pizza crust`, `I made this photo for a recipe blog post on how to make a cauliflower crust pizza`, `https://upload.wikimedia.org/wikipedia/commons/3/37/Cauliflower_pizza_crust_%2817024767906%29.jpg`)
+  ];
+  // we specify that variable recipe will take array of objects (type Recipe)
+  // we need to import it from ../recipe.model
 
 }
