2c45e6bcca4fa650b170a3c1807318ca36432206
ADMichnik <amek995@gmail.com>
2023-05-23 09:19:15 +0200

    passed info abot recipe clicked to recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
+
+  @Output() recipeSelectedTwice = new EventEmitter<Recipe>;
+
   recipes: Recipe[] = [
     new Recipe (`A Test Recipe`, `That is simply a test`, `https://upload.wikimedia.org/wikipedia/commons/9/96/Low-Carb_Zoodles_with_Bolognese.jpg`),
     new Recipe (`Cauliflower pizza crust`, `Recipe on how to make a cauliflower crust pizza`, `https://upload.wikimedia.org/wikipedia/commons/3/37/Cauliflower_pizza_crust_%2817024767906%29.jpg`)
@@ -14,4 +17,7 @@ export class RecipeListComponent {
   // we specify that variable recipe will take array of objects (type Recipe)
   // we need to import it from ../recipe.model
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelectedTwice.emit(recipe);
+  }
 }
