b88a4d786ae9d3405d011f294502fcd83883801d
ADMichnik <amek995@gmail.com>
2023-05-23 08:16:42 +0200

    added shoping edit, made nav work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-book';
+  
+  feature: string = 'recipe';
+
+  featureSet(feature: string) {
+    this.feature = feature;
+  }
 }
