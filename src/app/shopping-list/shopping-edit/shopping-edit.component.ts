6bbc6064bb38957f2fd1ad123baa10db1dad968c
ADMichnik <amek995@gmail.com>
2023-06-06 16:41:09 +0200

    populated shopping list service and made it work

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 
 @Component({
@@ -11,13 +12,15 @@ export class ShoppingEditComponent {
 
   @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef
   @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef
-  
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  ingredient: Ingredient;
+  
   onAddItem(){
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[] = [
-    new Ingredient (`Apples`, 5),
-    new Ingredient (`Tomatoes`, 10)
-  ];
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+  ingredients: Ingredient[];
+
+  ngOnInit(): void {
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingerientsChanged.subscribe((ingredients: Ingredient[]) => {
+      this.ingredients = ingredients;
+    })
   }
+
+  // onIngredientAdded(ingredient: Ingredient) {
+  //   this.shoppingListService.ingredients.push(ingredient);
+  // }
 }
