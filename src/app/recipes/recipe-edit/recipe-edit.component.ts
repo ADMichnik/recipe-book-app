6bca532ae534c8092206b8d460ebb7878f3063ee
ADMichnik <amek995@gmail.com>
2023-06-23 10:09:43 +0200

    linked recipe-edit comp

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.scss']
+  styleUrls: ['./recipe-edit.component.scss'],
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((paramas: Params) => {
+      this.id = +paramas.id;
+      this.editMode = paramas['id'] != null;
+    });
+  }
 }
